677eb2b1842cbedf80745432d23e5442a2ff0c98
dhyey18 <pateldhyey64@gmail.com>
2024-09-27 17:11:44 +0530

    Test: Add test for handling single number

--- tests/stringCalculator.test.ts
+++ tests/stringCalculator.test.ts
@@ -1,9 +1,13 @@
-import { add } from '../src/stringCalculator';
-
+import { add } from "../src/stringCalculator";
 
 // First Test: Handle Empty String
-describe('String Calculator', () => {
-  it('should return 0 for an empty string', () => {
-    expect(add('')).toBe(0);
+describe("String Calculator", () => {
+  it("should return 0 for an empty string", () => {
+    expect(add("")).toBe(0);
   });
+});
+
+// Second Test: Handle Single Number
+it("should return the number itself when one number is passed", () => {
+  expect(add("1")).toBe(1);
 });
