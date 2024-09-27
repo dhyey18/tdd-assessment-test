7d2f83de97ca43ff0bbb8352f3d38024872c5dd7
dhyey18 <pateldhyey64@gmail.com>
2024-09-27 17:24:56 +0530

    Feat: Support custom delimiters

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -2,9 +2,16 @@
 export function add(numbers: string): number {
   if (!numbers) return 0;
 
+  let delimiter = ",";
+  if (numbers.startsWith("//")) {
+    const parts = numbers.split("\n");
+    delimiter = parts[0].substring(2);
+    numbers = parts[1];
+  }
+
   const numArray = numbers
-    .replace(/\n/g, ",")
-    .split(",")
+    .replace(/\n/g, delimiter)
+    .split(delimiter)
     .map((num) => parseInt(num, 10));
   return numArray.reduce((sum, num) => sum + num, 0);
 }
