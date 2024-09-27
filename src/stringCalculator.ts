39e5abf6918473f031c94876864e8b30ac629c7e
dhyey18 <pateldhyey64@gmail.com>
2024-09-27 17:21:34 +0530

    Feat: Support newline as a number separator

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -2,6 +2,9 @@
 export function add(numbers: string): number {
   if (!numbers) return 0;
 
-  const numArray = numbers.split(",").map((num) => parseInt(num, 10));
+  const numArray = numbers
+    .replace(/\n/g, ",")
+    .split(",")
+    .map((num) => parseInt(num, 10));
   return numArray.reduce((sum, num) => sum + num, 0);
 }
