ac7cadc0342248853766a5ad41ff31eda405ad48
dhyey18 <pateldhyey64@gmail.com>
2024-09-27 17:15:44 +0530

    Feat: Sum two comma-separated numbers

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -1,5 +1,7 @@
 // implementing the minimal code to pass the test cases
 export function add(numbers: string): number {
   if (!numbers) return 0;
-  return parseInt(numbers, 10);
+
+  const numArray = numbers.split(",").map((num) => parseInt(num, 10));
+  return numArray.reduce((sum, num) => sum + num, 0);
 }
